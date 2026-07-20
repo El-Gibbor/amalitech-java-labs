5e6df92bfdd6aa434f50c4bc8ea53f464e1a3a6a
El-Gibbor <c.eke@alustudent.com>
2026-07-20 19:14:39 +0200

    Add JUnit tests for TransactionManager recording and querying transactions

---
+++ bank-account-management-system/src/test/java/services/TransactionManagerTest.java
@@ -0,0 +1,60 @@
+package services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import models.Transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TransactionManagerTest {
+    private TransactionManager transactionManager;
+
+    @BeforeEach
+    void setUp() {
+        transactionManager = new TransactionManager();
+    }
+
+    @Test
+    void addTransactionIncreasesTransactionCount() {
+        transactionManager.addTransaction(new Transaction("ACC001", "Deposit", 500, 500));
+        assertEquals(1, transactionManager.getTransactionCount());
+    }
+
+    @Test
+    void getTransactionsForAccountReturnsNewestFirst() {
+        Transaction first = new Transaction("ACC001", "Deposit", 500, 500);
+        Transaction second = new Transaction("ACC001", "Withdrawal", 100, 400);
+        transactionManager.addTransaction(first);
+        transactionManager.addTransaction(second);
+
+        Transaction[] history = transactionManager.getTransactionsForAccount("ACC001");
+
+        assertEquals(second.getTransactionId(), history[0].getTransactionId());
+        assertEquals(first.getTransactionId(), history[1].getTransactionId());
+    }
+
+    @Test
+    void calculateTotalDepositsSumsOnlyDeposits() {
+        transactionManager.addTransaction(new Transaction("ACC001", "Deposit", 500, 500));
+        transactionManager.addTransaction(new Transaction("ACC001", "Withdrawal", 100, 400));
+
+        assertEquals(500, transactionManager.calculateTotalDeposits("ACC001"));
+    }
+
+    @Test
+    void calculateTotalWithdrawalsSumsOnlyWithdrawals() {
+        transactionManager.addTransaction(new Transaction("ACC001", "Deposit", 500, 500));
+        transactionManager.addTransaction(new Transaction("ACC001", "Withdrawal", 100, 400));
+
+        assertEquals(100, transactionManager.calculateTotalWithdrawals("ACC001"));
+    }
+
+    @Test
+    void getTransactionCountByAccountFiltersByAccount() {
+        transactionManager.addTransaction(new Transaction("ACC001", "Deposit", 500, 500));
+        transactionManager.addTransaction(new Transaction("ACC002", "Deposit", 300, 300));
+
+        assertEquals(1, transactionManager.getTransactionCountByAccount("ACC001"));
+    }
+}
